c575488a2814a5a58fe0e1c8f1b2d9c6c98181c5
Tomáš Heger <heger.tomas@gmail.com>
2018-02-07 00:40:20 +0100

    Add models.

--- spearch/admin.py
+++ spearch/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from searchapp.models import Institution, Speaker, Speech
+
+admin.site.register(Institution)
+admin.site.register(Speaker)
+admin.site.register(Speech)
--- spearch/models.py
+++ spearch/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Institution(models.Model):
+    name = models.CharField(max_length=100)
+
+class Speaker(models.Model):
+    name = models.CharField(max_length=100)
+    url_psp = models.URLField()
+
+class Speech(models.Model):
+    institution = models.ForeignKey('Institution', on_delete=models.CASCADE, blank=False)
+    speaker = models.ForeignKey('Speaker', on_delete=models.CASCADE)
+    speaker_title = models.CharField(max_length=100)
+    #date = models.DateField()
+    speech = models.TextField()
+
